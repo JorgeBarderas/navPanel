4318eee5ed0ae37deb2a47f699dbef6c6e609323
Jorge Barderas <jorgebarderas@gmail.com>
2015-01-26 08:08:32 +0100

    Development process

--- custom/custom.nav.js
+++ custom/custom.nav.js
@@ -1,14 +1,18 @@
 var custom = {
     init_main_sections: function (a_sections) {
         $("body").append("<div id='custom-favs'>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</div>")
-        a_sections.push({code: 'favs', type: 'expand', title: 'Favorites', description: 'Content favs', id_content: 'custom-favs'});
+        a_sections.push({code: 'favs', type: 'expand-link-tiles', title: 'Favorites', description: 'Content favs', id_content: 'custom-favs'});
         $("body").append("<div id='custom-admin'>Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</div>")
-        a_sections.push({code: 'admin', type: 'expand', title: 'Administration', description: 'Administration items', id_content: 'custom-admin'});
+        a_sections.push({code: 'admin', type: 'expand-link', title: 'Administration', description: 'Administration items', id_content: 'custom-admin'});
+        a_sections.push({code: 'files', type: 'button', title: 'Files', description: 'File list', id_content: ''});
     },
     init_foot_sections: function (a_sections) {
         $("body").append("<div id='custom-eproc'>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</div>")
         a_sections.push({code: 'msg-eproc', type: 'expand-link', title: 'e-Procurement messages', description: 'e-Procurement messages', id_content: 'custom-eproc'});
+        $("body").append("<div id='custom-chat'>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</div>")
+        a_sections.push({code: 'msg-chat', type: 'expand-refresh-info', title: 'Chat', description: 'Chat rooms', id_content: 'custom-chat'});
+        a_sections.push({code: 'mails', type: 'button', title: 'e-Mails', description: 'e-Mail list', id_content: ''});
         $("body").append("<div id='custom-personal'>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</div>")
-        a_sections.push({code: 'msg-personal', type: 'expand-link', title: 'Personal agenda', description: 'Personal agenda', id_content: 'custom-personal'});
+        a_sections.push({code: 'msg-personal', type: 'expand-link-tiles', title: 'Personal agenda', description: 'Personal agenda', id_content: 'custom-personal'});
     }
 };
--- views/navPanel.view.js
+++ views/navPanel.view.js
@@ -2,48 +2,43 @@
 window.JST = {};
 
 window.JST['nav/section/expand'] = _.template(
-      "<div class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %><img src='img/expand.svg' class='svg-inject expand' /></div>"+
-      "<div class='nav-panel-section-<%= type %>-content'>"+
+      "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %><img src='img/expand.svg' class='svg-inject expand' /></a>"+
+      "<div class='nav-panel-section-<%= type %>-content panel-content'>"+
       "</div>" 
+);
+window.JST['nav/section/button'] = _.template(
+      "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %><img src='img/next.svg' class='svg-inject next' /></a>"
 );
 window.JST['nav/section/expand-link'] = _.template(
       "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %>"+
         "<img src='img/expand.svg' class='svg-inject expand' />"+
       "</a>"+
       "<a href='#' class='nav-section-title-button'><img src='img/next.svg' class='svg-inject link' /></a>"+
-      "<div class='nav-panel-section-<%= type %>-content'>"+
+      "<div class='nav-panel-section-<%= type %>-content panel-content'>"+
+      "</div>" 
+);
+window.JST['nav/section/expand-refresh-info'] = _.template(
+      "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %>"+
+        "<img src='img/expand.svg' class='svg-inject expand' />"+
+      "</a>"+
+      "<a href='#' class='nav-section-title-info-button'><img src='img/refresh.svg' class='svg-inject refresh' /></a>"+
+      "<div class='nav-section-title-info'>23</div>"+
+      "<div class='nav-panel-section-<%= type %>-content panel-content'>"+
+      "</div>" 
+);
+window.JST['nav/section/expand-link-tiles'] = _.template(
+      "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %>"+
+        "<img src='img/expand.svg' class='svg-inject expand' />"+
+      "</a>"+
+      "<a href='#' class='nav-section-title-button'><img src='img/next.svg' class='svg-inject link' /></a>"+
+      "<div class='nav-panel-section-<%= type %>-preview'></div>"+
+      "<div class='nav-panel-section-<%= type %>-content panel-content'>"+
       "</div>" 
 );
 window.JST['nav/section/user'] = _.template(
-      "<div class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %><img src='img/user.svg' class='svg-inject user' /></div>"+
-      "<div class='nav-panel-section-<%= type %>-content'>"+
+      "<a href='#' class='nav-section-title nav-panel-section-<%= type %>-title'><%= title %><img src='img/user.svg' class='svg-inject user' /></a>"+
+      "<div class='nav-panel-section-<%= type %>-content panel-content'>"+
       "</div>" 
-);
-
-
-window.JST['nav/report'] = _.template(
-      "<a href='<%= url %>' class='nav-panel-section-reports-report><%= text %></a>"
-);
-window.JST['nav/section/agenda'] = _.template(
-      "<div class='nav-panel-section-messages-title'>"+
-        "<%= text %>"+
-        "<div class='nav-panel-section-messages-title-button'><img src='img/next.svg' class='svg-inject' /></div>"+
-      "</div>"+
-      "<div class='nav-panel-section-messages-tiles'></div>"+
-      "<div class='nav-panel-section-messages-content'></div>"
-);
-window.JST['nav/messages/tile'] = _.template(
-      "<label class='nav-panel-section-messages-tile message-<%= type %>'></label>"
-);
-window.JST['nav/messages/info'] = _.template(
-      "<div class='nav-panel-section-messages-info'>"+
-        "<label class='info-label message-<%= type %>'></label>"+
-        "<div class='info-title'><%= title %></div>"+
-        "<div class='info-description'><%= description %></div>"+
-      "</div>"
-);
-window.JST['nav/setting'] = _.template(
-      "<a href='<%= url %>' class='nav-panel-section-user-setting setting-<%= type %>'><%= text %></a>"
 );
 
 
@@ -60,72 +55,19 @@ var NP_main_sections_model = Backbone.Model.extend({
 var NP_sections_collection = Backbone.Collection.extend({
   model: NP_main_sections_model
 });
-/*REPORTS*/
-var Reports_model = Backbone.Model.extend({
-  id: '',
-  text: '',
-  url: ''
-});
-var Reports_collection = Backbone.Collection.extend({
-  model: Reports_model
-});
-/*MESSAGES*/
-var Agenda_model = Backbone.Model.extend({
-  id: '',
-  text: '',
-  messages: Messages_collection
-});
-var Agendas_collection = Backbone.Collection.extend({
-  model: Agenda_model
-});
-
-var Message_model = Backbone.Model.extend({
-  code: '',
-  index: -1,
-  number: -1,
-  type: '',
-  title: '',
-  summary: '',
-  
-  initialize: function (args) {
-    this.code = args.type+"_"+args.index+"_"+args.number;
-  }
-});
-var Messages_collection = Backbone.Collection.extend({
-  model: Message_model
-});
-/*SETTINGS*/
-var Settings_model = Backbone.Model.extend({
-  id: '',
-  text: '',
-  url: '',
-  type: ''
-});
-var Settings_collection = Backbone.Collection.extend({
-  model: Settings_model
-});
 
 /*VIEWS***************************************************************************/
 var NAVpanel_view = Backbone.View.extend({
 
   tagName: "div",
   className: "nav-panel",
-  collection: NP_sections_collection,
   main_sections: [],
   foot_sections: [],
-  agendas: [],
-  agendas_views: [],
-  settings: [],
-  settings_view: null,
   section_views: [],
-  userName: "",
 
   initialize: function(args) {
-    this.userName = args.user;
     this.main_sections = args.main_sections;
     this.foot_sections = args.foot_sections;
-    this.agendas = args.agendas;
-    this.settings = args.settings;
   },
 
   render: function() {
@@ -133,13 +75,15 @@ var NAVpanel_view = Backbone.View.extend({
     view.$el.addClass(view.className);
     //main
     _.each(view.main_sections.models,function(section) {
-      var nS_v = new NAVsection_view({model: section, $content: $("#"+section.get("id_content")), collapsed: false});
+      var id_content = section.get("id_content");
+      var nS_v = new NAVsection_view({model: section, $content: (id_content!="")?$("#"+id_content):[], collapsed: false});
       view.$el.children("#main-panel").append(nS_v.render().$el);
       view.section_views.push(nS_v);
     });
     //foot
     _.each(view.foot_sections.models,function(section) {
-      var nS_v = new NAVsection_view({model: section, $content: $("#"+section.get("id_content")), collapsed: true});
+      var id_content = section.get("id_content");
+      var nS_v = new NAVsection_view({model: section, $content: (id_content!="")?$("#"+id_content):[], collapsed: true});
       if (section.get("type") != "user") {
         view.$el.children("#foot-panel").prepend(nS_v.render().$el);
       } else {
@@ -147,16 +91,6 @@ var NAVpanel_view = Backbone.View.extend({
       }
       view.section_views.push(nS_v);
     });
-    /*
-
-    _.each(view.agendas.models,function(agenda){
-      var nA_v = new Agenda_view({model: agenda});
-      view.$el.find(".nav-panel-section-agendas").append(nA_v.render().$el);
-      view.agendas_views.push(nA_v);
-    });
-    view.settings_view = new Settings_view({collection: view.settings, el: view.$(".nav-panel-section-user")[0]});
-    view.settings_view.render();
-    */
     $(window).resize(function () {view.resize()});
     view.resize();
     return this;
@@ -191,12 +125,18 @@ var NAVsection_view = Backbone.View.extend({
       .addClass("nav-panel-section")
       .addClass("nav-panel-section-" + view.model.get("type"))
       .append(window.JST['nav/section/'+view.model.get("type")](view.model.attributes));
-    view.$content.appendTo(view.$el.find(".nav-panel-section-"+view.model.get("type")+"-content"));
+    if (view.$content.length > 0) {
+      view.$content.appendTo(view.$el.find(".nav-panel-section-"+view.model.get("type")+"-content"));
+    } else {
+      view.collapsed = true;
+    }
 
     if (view.collapsed) {
+      view.$el.addClass("collapsed").removeClass("expanded");
       view.$(".nav-panel-section-"+view.model.get("type")+"-title").addClass("title-collapsed");
       view.$(".nav-panel-section-"+view.model.get("type")+"-content").hide();
     } else {
+      view.$el.addClass("expanded").removeClass("collapsed");
       view.$(".nav-panel-section-"+view.model.get("type")+"-title").addClass("title-expanded");
       view.$(".nav-panel-section-"+view.model.get("type")+"-content").show();
     }
@@ -208,125 +148,11 @@ var NAVsection_view = Backbone.View.extend({
     var hidden = view.$(".nav-panel-section-"+view.model.get("type")+"-content").css("display") == "none";
     view.$(".nav-panel-section-"+view.model.get("type")+"-content").slideToggle(function() {$(window).resize();});
     if (hidden) {
+      view.$el.addClass("expanded").removeClass("collapsed");
       view.$(".nav-panel-section-"+view.model.get("type")+"-title").removeClass("title-collapsed").addClass("title-expanded");
     } else {
+      view.$el.addClass("collapsed").removeClass("expanded");
       view.$(".nav-panel-section-"+view.model.get("type")+"-title").removeClass("title-expanded").addClass("title-collapsed");
-    }
-  }
-
-});
-/*AGENDA VIEW*/
-var Agenda_view = Backbone.View.extend({
-
-  tagName: "div",
-  className: "nav-panel-section-agenda",
-  model: Agenda_model,
-
-  events: {
-    "click": "clickHeader"
-  },
-
-  render: function() {
-    var view = this;
-    view.$el
-        .attr("id", view.model.get("id"))
-        .addClass(view.className)
-        .append(window.JST['nav/section/agenda'](view.model.attributes));
-    view.$(".nav-panel-section-messages-title").addClass("title-collapsed");
-    var cM = view.model.get("messages");
-    if (cM.length > 0) {
-      _.each(cM, function(message) {
-        view.$(".nav-panel-section-messages-tiles").append(window.JST['nav/messages/tile'](message));
-        view.$(".nav-panel-section-messages-content").append(window.JST['nav/messages/info'](message));
-      });
-    }
-    return this;
-  },
-
-  clickHeader: function() {
-    var view = this;
-    var hidden = view.$(".nav-panel-section-messages-content").css("display") == "none";
-    view.$(".nav-panel-section-messages-content").slideToggle(function() {$(window).resize();});
-    if (hidden) {
-      view.$(".nav-panel-section-messages-title").removeClass("title-collapsed").addClass("title-expanded");
-      view.$(".nav-panel-section-messages-tiles").slideUp();
-    } else {
-      view.$(".nav-panel-section-messages-title").removeClass("title-expanded").addClass("title-collapsed");
-      view.$(".nav-panel-section-messages-tiles").slideDown();
-    }
-  }
-
-});
-/*MESSAGES VIEW*/
-var Messages_view = Backbone.View.extend({
-
-  tagName: "div",
-  className: "nav-panel-section-messages",
-  collection: Messages_collection,
-
-  events: {
-    "click": "clickHeader"
-  },
-
-  render: function() {
-    var view = this;
-    view.$(".nav-panel-section-messages-title").addClass("title-collapsed");
-    view.$(".nav-panel-section-messages-tiles").addClass("tiles-collapsed");
-    if (view.collection.length > 0) {
-      view.$el.show();
-      _.each(view.collection.models, function(message) {
-        view.$(".nav-panel-section-messages-tiles").append(window.JST['nav/messages/tile'](message.attributes));
-        view.$(".nav-panel-section-messages-content").append(window.JST['nav/messages/info'](message.attributes));
-      });
-    }
-    return this;
-  },
-
-  clickHeader: function() {
-    var view = this;
-    var hidden = view.$(".nav-panel-section-messages-content").css("display") == "none";
-    view.$(".nav-panel-section-messages-content").slideToggle(function() {$(window).resize();});
-    if (hidden) {
-      view.$(".nav-panel-section-messages-title").removeClass("title-collapsed").addClass("title-expanded");
-      view.$(".nav-panel-section-messages-tiles").removeClass("tiles-collapsed").addClass("tiles-expanded");
-    } else {
-      view.$(".nav-panel-section-messages-title").removeClass("title-expanded").addClass("title-collapsed");
-      view.$(".nav-panel-section-messages-tiles").removeClass("tiles-expanded").addClass("tiles-collapsed");
-    }
-  }
-
-});
-/*SETTINGS VIEW*/
-var Settings_view = Backbone.View.extend({
-
-  tagName: "div",
-  className: "nav-panel-section-user",
-  collection: Settings_collection,
-
-  events: {
-    "click": "clickHeader"
-  },
-
-  render: function() {
-    var view = this;
-    view.$(".nav-panel-section-user-title").addClass("title-collapsed");
-    if (view.collection.length > 0) {
-      view.$el.show();
-      _.each(view.collection.models, function(setting) {
-        view.$(".nav-panel-section-user-content").append(window.JST['nav/setting'](setting.attributes));
-      });
-    }
-    return this;
-  },
-
-  clickHeader: function() {
-    var view = this;
-    var hidden = view.$(".nav-panel-section-user-content").css("display") == "none";
-    view.$(".nav-panel-section-user-content").slideToggle(function() {$(window).resize();});
-    if (hidden) {
-      view.$(".nav-panel-section-user-title").removeClass("title-collapsed").addClass("title-expanded");
-    } else {
-      view.$(".nav-panel-section-user-title").removeClass("title-expanded").addClass("title-collapsed");
     }
   }
 
